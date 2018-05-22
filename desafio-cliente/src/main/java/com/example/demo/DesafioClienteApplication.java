fa7835094f6a9c6065704df73ca866b4264536ff
Thiago Padilha <thiago.moura@gmail.redes.unb.br>
2018-05-22 16:46:11 -0300

    [UPDATE]

---
+++ desafio-cliente/src/main/java/com/example/demo/DesafioClienteApplication.java
@@ -0,0 +1,31 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@SpringBootApplication
+@EnableOAuth2Sso
+@Controller
+public class DesafioClienteApplication extends WebSecurityConfigurerAdapter{
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioClienteApplication.class, args);
+	}
+	
+	
+	@Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.antMatcher("/**")
+          .authorizeRequests()
+          .antMatchers("/", "/login**")
+          .permitAll()
+          .anyRequest()
+          .authenticated();
+    }
+	
+}
